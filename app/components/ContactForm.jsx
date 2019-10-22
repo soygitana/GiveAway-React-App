76b5c5c2bda34d5e4c49b276aafcfd9b7d80603c
Lizon <v-Paula.Lizon@lionbridge.com>
2019-10-22 15:30:15 +0200

    form validation

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -1,93 +1,80 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import "../styles/main.scss";
 import axios from 'axios'
 
-const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
-const validateForm = (errors) => {
-    let valid = true;
-    Object.values(errors).forEach(
-        (val) => val.length > 0 && (valid = false)
-    );
-    return valid;
+import SuccessMessage from './SuccessMessage.jsx';
+
+
+function validate(name, email, message) {
+    
+    //  storing errors for all fields in a signle array
+
+    const errors = [];
+
+    // regular expression for email validation
+
+    function emailIsValid (email) {
+        const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+        return validEmailRegex.test(email)
+      }
+
+
+    if (name.length === 0) {
+        errors.push("name can't be empty");
+        console.log('invalid name')
+    }
+    if (email.length === 0 || !emailIsValid(email)) {
+        errors.push("invalid email");
+        console.log('incorrect email')
+    }
+
+    if (message.length < 120) {
+        errors.push("message should be at least 120 characters long");
+        console.log('invalid message')
+    }
+
+    return errors;
 }
 
 
-const withSuccessHandling = WrappedComponent => ({ showSuccess, children }) => {
-    return (
-      <WrappedComponent>
-        {showSuccess && <div className="success">Wiadomość została wysłana! <br/>
-         Wkrótce się z Tobą skontaktujemy.</div>}
-        {children}
-      </WrappedComponent>
-    );
-  };
-
-  const DivWithSuccessHandling = withSuccessHandling(({children}) => <div>{children}</div>)
-
-
-
-class ContactForm extends Component {
-    constructor(props) {
-        super(props);
+class SignUpForm extends React.Component {
+    constructor() {
+        super();
         this.state = {
-            fullName: null,
-            email: null,
-            message: null,
+            errors: [],
             showSuccess: false,
-            errors: {
-                fullName: '',
-                email: '',
-                message: '',
-            }
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
-    handleChange = (event) => {
-        event.preventDefault();
-        const { name, value } = event.target;
-        let errors = this.state.errors;
 
-        switch (name) {
-            case 'fullName':
-                errors.fullName =
-                    value.length < 5
-                        ? 'name must be 5 characters long!'
-                        : '';
-                break;
-            case 'email':
-                errors.email =
-                    validEmailRegex.test(value)
-                        ? ''
-                        : 'email is not valid!';
-                break;
-            case 'message':
-                errors.message =
-                    value.length < 120
-                        ? 'message must be 120 characters in length'
-                        : '';
-                break;
-            default:
-                break;
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const name = ReactDOM.findDOMNode(this._nameInput).value;
+        const email = ReactDOM.findDOMNode(this._emailInput).value;
+        const message = ReactDOM.findDOMNode(this._messageInput).value;
+
+
+        const errors = validate(name, email, message);
+        if (errors.length > 0) {
+            this.setState({ errors });
+            console.log("invalid form!")
+            return;
         }
 
-        this.setState({ errors, [name]: value });
-    }
-
- 
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        if (validateForm(this.state.errors)) {
-            console.log('Valid Form');
-            toggleSuccess = () => {
-                this.setState((prevState, props,) => {
-                  return { showSuccess: !prevState.showSuccess }
-                });
-              };
-        } else {
-            console.log('Invalid Form')
+        if (errors.length === 0) {
+            // submit the data...
+            console.log("valid form!")
+            this.setState({
+                showSuccess: "true"
+            });
+            this.myFormRef.reset();
         }
+
     }
 
     render() {
@@ -95,44 +82,49 @@ class ContactForm extends Component {
         return (
             <>
 
-         <DivWithSuccessHandling showSuccess={this.state.showSuccess} /> 
+                <SuccessMessage showSuccess={this.state.showSuccess} />
 
-            <div className="container-flex contact-form">
-                <form onSubmit={this.handleSubmit} noValidate>
-
-                    <div className="container-flex">
-                        <div className="box-flex">
-                            <label htmlFor="fullName"><p className="input-title"> Wpisz swoje imię:</p></label>
-                            <input type='text' name='fullName' onChange={this.handleChange} noValidate />
-                            {errors.fullName.length > 0 &&
-                                <span className="error">Podane imię jest nieprawidłowe</span>}
+                <div className="container-flex contact-form">
+                    <form ref={(el) => this.myFormRef = el} onSubmit={this.handleSubmit}>
+                        <div className="container-flex">
+                            <div className="box-flex">
+                                <label htmlFor="name"><p className="input-title"> Wpisz swoje imię:</p></label>
+                                <input name='name' ref={nameInput => (this._nameInput = nameInput)}
+                                    type="text"
+                                    placeholder="name" />
+                                {errors.length > 0 &&
+                                    <span className="error">Pole imię nie może być puste</span>}
+                            </div>
+                            <div className="box-flex">
+                                <label htmlFor="email"><p className="input-title">Wpisz swoj email:</p></label>
+                                <input name='email' ref={emailInput => (this._emailInput = emailInput)}
+                                    type="text"
+                                    placeholder="email" />
+                                {errors.length > 0 &&
+                                    <span className="error">Podany email jest nieprawidłowy</span>}
+                            </div>
                         </div>
                         <div className="box-flex">
-                            <label htmlFor="email"><p className="input-title">Wpisz swoj email:</p></label>
-                            <input type='email' name='email' onChange={this.handleChange} noValidate />
-                            {errors.email.length > 0 &&
-                               <span className="error">Podany email jest nieprawidłowy</span>}
+                            <label htmlFor="message"><p className="input-title"> Wpisz swoją wiadomość:</p></label>
+                            <textarea name='message' ref={messageInput => (this._messageInput = messageInput)}
+                                type="text"
+                                placeholder="message" />
+                            {errors.length > 0 &&
+                                <span className="error">Wiadomość musi mieć conajmniej 120 znaków</span>}
                         </div>
-                    </div>
-                    <div className="box-flex">
-                        <label htmlFor="message"><p className="input-title"> Wpisz swoją wiadomość:</p></label>
-                        <textarea type='text' name='message' onChange={this.handleChange} noValidate />
-                        {errors.message.length > 0 &&
-                             <span className="error">Wiadomość musi mieć conajmniej 120 znaków</span>}
-                    </div>
-                    <div className='info'>
-                    </div>
-                    <div className='container-flex container-btn'>
-                        <button onClick={this.toggleSuccess} className="submitBtn">Wyślij</button>
-                    </div>
+                        <div className='info'>
+                        </div>
+                        <div className='container-flex container-btn'>
+                            <button type="submit" className="submitBtn">Wyślij</button>
+                        </div>
 
-                </form>
-            </div>
-</>
+                    </form>
+                </div>
+            </>
         );
     }
 }
-export default ContactForm;
+export default SignUpForm;
 
 
 
---
+++ app/components/SuccessMessage.jsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import "../styles/main.scss";
+
+
+// creating Success Component to show success message
+
+const withSuccessHandling = WrappedComponent => ({ showSuccess, children }) => {
+    return (
+        <WrappedComponent>
+            {showSuccess && <div className="success">Wiadomość została wysłana! <br />
+                Wkrótce się z Tobą skontaktujemy.</div>}
+            {children}
+        </WrappedComponent>
+    );
+};
+
+const SuccessMessage = withSuccessHandling(({ children }) => <div>{children}</div>)
+
+
+export default SuccessMessage;
